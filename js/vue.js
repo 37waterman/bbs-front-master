c94a12bd9bd7601085504d41c373ab9db471a735
li <528085390@qq.com>
2025-10-10 22:37:54 +0800

    search

--- js/vue.js
+++ js/vue.js
@@ -220,10 +220,6 @@ new Vue({
 })
 
 
-
-
-
-
 new Vue({
     el: '#user-center',
     data() {
@@ -247,12 +243,39 @@ new Vue({
                         'token': token
                     }
                 })
-                    .then(response => {
-                        this.information.avatarInput = response.data.data.avatarUrl;
-                        this.information.accountInput = response.data.data.username;
-                        this.information.emailInput = response.data.data.email;
-                    })
+                .then(response => {
+                    this.information.avatarInput = response.data.data.avatarUrl;
+                    this.information.accountInput = response.data.data.username;
+                    this.information.emailInput = response.data.data.email;
+                })
 
         }
     }
+})
+
+new Vue({
+    el: '#search_content',
+    data() {
+        return {
+            searchInfo: '',
+        }
+    },
+    methods: {
+        search() {
+            this.searchInfo = document.getElementById('search').value;
+            axios
+                .get('http://8.148.233.225:8081/posts', {
+                    params: {
+                        title: this.searchInfo
+                    }
+                })
+                .then(response => {
+                    alert(response.data);
+                })
+                .catch(err => {
+
+                });
+        }
+    }
+
 })
